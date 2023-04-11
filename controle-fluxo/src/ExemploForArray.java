683022ee76fac51b27887507f47120616ab99f1c
ramisesreis <ramisesreis@outlook.com>
2023-04-11 08:35:44 -0300

    add

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,17 @@
+import java.util.jar.Manifest;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {
+            "Felipe","Jonas","Julia","Marcos"};
+
+            //for(int x=0; x<alunos.length; x++){
+            //System.out.println("O aluno no índice x=" + x + " é " + alunos [x]);
+            for(String aluno: alunos){
+                System.out.println("Nome do aluno é: "+ aluno);
+        }
+    }
+}
+    
+
+
